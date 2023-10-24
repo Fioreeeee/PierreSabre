32300b14a1dbf622e4e08efff20706f99f815b05
Fioreeeee <francoisfiore4@gmail.com>
2023-10-24 18:39:00 +0200

    2eme test TP4

--- src/personnages/Humain.java
+++ src/personnages/Humain.java
@@ -1,5 +1,23 @@
 package personnages;
 
 public class Humain {
+	private String nom;
+    private String boissonFavorite;
+    private int argent;
+    
+    public Humain(String nom, String boissonFavorite, int argent) {
+        this.nom = nom;
+        this.boissonFavorite = boissonFavorite;
+        this.argent = argent;
+    }
+    
+    public String getNom() {
+        return nom;
+    }
 
+    public int getArgent() {
+        return argent;
+    }
+    
+    
 }
