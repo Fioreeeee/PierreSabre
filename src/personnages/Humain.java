3c490c06707c6066a27a60fc85f1e45b80217934
Fioreeeee <francoisfiore4@gmail.com>
2023-10-24 18:51:09 +0200

    3eme test TP4

--- src/personnages/Humain.java
+++ src/personnages/Humain.java
@@ -19,5 +19,24 @@ public class Humain {
         return argent;
     }
     
+    private void parler(String texte) {
+        System.out.println(nom + " - " + texte);
+    }
+    
+    public void direBonjour() {
+        parler("Bonjour ! Je m'appelle " + nom + " et j'aime boire du " + boissonFavorite + ".");
+    }
+
+    public void boire() {
+        parler("Mmmm, un bon verre de " + boissonFavorite + " ! GLOUPS !");
+    }
+    
+    public void gagnerArgent(int gain) {
+        argent += gain;
+    }
+
+    public void perdreArgent(int perte) {
+        argent -= perte;
+    }
     
 }
